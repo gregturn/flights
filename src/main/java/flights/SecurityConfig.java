88eb168e4e0644a6c3c4916a10bfa508d57c5c7b
Greg Turnquist <gturnquist@gopivotal.com>
2014-10-08 10:47:17 -0500

    Use autoconfiguration

--- src/main/java/flights/SecurityConfig.java
+++ src/main/java/flights/SecurityConfig.java
@@ -1,15 +1,13 @@
 package flights;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
@@ -17,27 +15,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/login").permitAll()
-			.and()
-				.formLogin()
-				.loginPage("/login")
-				.loginProcessingUrl("/login")
-			.and()
-				.exceptionHandling()
-					.accessDeniedPage("/login?auth_error=true")
-			.and()
-				.csrf()
-					.requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize")).disable();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-				.withUser("greg").password("password").roles("USER");
+				.withUser("walt").password("marceline").roles("USER");
 	}
 }
